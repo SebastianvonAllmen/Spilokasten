e110a9b916b198ee538af0939b71c719da254948
SebastianvonAllmen <sebastian.v.allmen@gmail.com>
2021-12-07 13:36:08 +0100

    implementing class row

---
+++ classes/row.py
@@ -0,0 +1,7 @@
+class row:
+    def __init__(self, emblem1, emblem2, emblem3):
+        self.content = []
+        self.content.append(emblem1)
+        self.content.append(emblem2)
+        self.content.append(emblem3)
+
--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from classes.emblem import emblem
+from classes.row import row
 
 def main():
     melone = emblem("m", 20, 0.1)
+    citrone = emblem("c", 20, 0.1)
+    kirsche = emblem("k", 20, 0.1)
+    traube = emblem("t", 20, 0.1)
+    orange = emblem("O", 20, 0.1)
+    bar = emblem("BAR", 20, 0.1)
+    seven = emblem("7", 20, 0.1)
+    Bonus = emblem("BBB", 20, 0.1)
+    glocke = emblem("G", 20, 0.1)
+    dia = emblem("D", 20, 0.1)
+
+    crtrow = row(melone, orange, dia)
+
 
     print(melone.character)
 
