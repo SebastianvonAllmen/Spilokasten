f036bd9e0b643ea802aa84fbeed7c6169fd194ab
SebastianvonAllmen <sebastian.v.allmen@gmail.com>
2021-12-07 13:14:53 +0100

    file config complete

---
+++ classes/symbol.py
@@ -0,0 +1,5 @@
+class symbol:
+    def __init__(self, character, net, chance):
+        self.character = character
+        self.net = net
+        self.chance = chance
---
+++ src/main.py
@@ -0,0 +1,20 @@
+import symbol
+
+def main():
+    print("hello")
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
